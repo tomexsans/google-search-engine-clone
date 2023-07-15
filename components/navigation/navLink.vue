ead7ab08c2dbdca9384cd4a2ebd98aba1e13846a
Tom Evaristo <t2t.exe@gmail.com>
2023-07-15 21:15:52 +0800

    Added Component NavLink

---
+++ components/navigation/navLink.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+const clickhandler = () => alert('This is a google Clone')
+</script>
+
+<template>
+    <a @click.prevent="clickhandler" href="#">
+        <slot></slot>
+    </a>
+</template>
+
+<style scoped>
+[rounded-link] {
+    @apply rounded-full hover:bg-slate-200 h-[50px] w-[50px]
+    flex justify-center items-center;
+}
+</style>
